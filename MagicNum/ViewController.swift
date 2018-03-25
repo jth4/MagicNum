2b2114767f7ae70e60bff1e21105f2ee3359fa7b
jth4 <jth4@Spiderman.local>
2018-03-25 00:54:14 -0700

    Init

--- MagicNum/ViewController.swift
+++ MagicNum/ViewController.swift
@@ -10,16 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var wordInputField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
+  
+    
+    var wordValueArray = [Int]()
+    
+    // Ad banner and interstitial views
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        wordInputField.delegate = self
+        
     }
 
 
+    @IBAction func calculateTapped(_ sender: Any) {
+        let trimmedInputText : String = wordInputField.text!.lowercased().replacingOccurrences(of: " ", with: "")
+        let noPunc = trimmedInputText.components(separatedBy: CharacterSet.punctuationCharacters).joined()
+        var noNum = noPunc.components(separatedBy: CharacterSet.decimalDigits).joined()
+        
+        for letter in noNum.utf8 {
+            let letterValue = Int(letter - 96)
+            wordValueArray.append(letterValue)
+        }
+        
+        let summedArray = wordValueArray.reduce(0, {$0 + $1})
+        textView.text = "The Magic Number is \(summedArray)"
+        wordValueArray = [Int]()
+    }
 }
 
+extension ViewController : UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+}
